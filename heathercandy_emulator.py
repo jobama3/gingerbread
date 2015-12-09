e2faf3663c3bdbebdf696d508c481821289b1528
hgesserit <heather.nakama@live.com>
2015-12-09 07:43:05 -0800

    Add emulator and args to main xmas script

---
+++ heathercandy_emulator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+# Emulator for fadecandy
+
+import pygame
+
+def put_pixels(pixels):
+	print pixels
--- xmas_messing_around.py
+++ xmas_messing_around.py
@@ -4,14 +4,19 @@
 #   xmas.py <input sequence> <audio file>
 
 import sys
+import getopt
 import time
 import pygame
 import random
 import opc
+import heathercandy_emulator
 
-helpstring = "xmas.py <input sequence> <audio file>"
-
-useMS = False
+####################################################################
+# Print help
+def usage():
+  print "xmas.py --sequence <sequence file> --audio <audio file>"
+  print "   [optional] --emulate --silent"
+####################################################################
 
 #fadecandy constants
 numLEDs = 50
@@ -20,26 +25,84 @@ white = [ (255,255,255) ] * numLEDs
 green = [ (255,0,0) ] * numLEDs
 red = [ (0,255,0) ] * numLEDs
 
+#parse and validate args
+emulate = False
+silent = False
+useMS = False
+
+try:                                
+  opts, args = getopt.getopt(sys.argv[1:], "hs:a:es", ["help", "sequence=", "audio=", "emulate", "silent"])
+except getopt.GetoptError:
+  print "Error parsing args"
+  usage()
+  sys.exit(2)
+
+for opt, arg in opts:
+  if opt in ("-h", "--help"):
+    usage()
+    sys.exit()
+  elif opt in ("-s", "--sequence"):
+    sequence_file = arg
+  elif opt in ("-a", "--audio"):
+    audio_file = arg
+  elif opt in ("-e", "--emulate"):
+    emulate = True
+    print "Emulating"
+  elif opt in ("-s", "--silent"):
+    silent = True
+
+try:
+  print "Sequence file: ", sequence_file
+  if not silent:
+    print "Audio file: ", audio_file
+  else:
+    print "Running in silent mode"
+  print
+except NameError:
+  usage()
+  sys.exit(2)
 
 ####################################################################
 # Do whatever we need to do to reset and prep the pi
 def initialize():
-  #create client for fadecandy
-  client = opc.Client('localhost:7890')
-  
   print("I'm initializing!")
-  return client;
+  
+  #create client for fadecandy
+  if not emulate:
+    global client
+    client = opc.Client('localhost:7890')
 ####################################################################
 
 
 #####################################################################
 # Open the input sequnce file and read/parse it
 def getmusicsequence():
-  with open(sys.argv[1],'r') as f:
+  with open(sequence_file,'r') as f:
     seq_data = f.readlines()
     for i in range(len(seq_data)):
       seq_data[i] = seq_data[i].rstrip()
   return seq_data
+####################################################################
+
+
+#####################################################################
+# Open the input sequnce file and read/parse it
+def startaudio():
+  if not silent:
+    # Load and play the music
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play()
+####################################################################
+
+
+#####################################################################
+# Open the input sequnce file and read/parse it
+def put_pixels(pixels):
+  if emulate:
+    heathercandy_emulator.put_pixels(pixels)
+  else:
+    client.put_pixels(pixels)
 ####################################################################
 
 
@@ -54,24 +117,10 @@ def getcurtime():
 ####################################################################
 
 
-num_args = len(sys.argv) - 1
-if num_args != 2:
-  print(num_args, "args passed in, should be 2")
-  print(helpstring)
-  sys.exit()
-
-client = initialize()
-
-# Open the input sequnce file and read/parse it
+initialize()
 seq_data = getmusicsequence()
 
-# Initialize light states
-lights = [False for i in range(8)]
-
-# Load and play the music
-pygame.mixer.init()
-pygame.mixer.music.load(sys.argv[2])
-pygame.mixer.music.play()
+startaudio()
 
 # Start sequencing
 start_time = getcurtime()
@@ -99,10 +148,10 @@ while True :
     if command == "END":
       sys.exit()
     elif command == "HEATHER":
-      client.put_pixels(red)
+      put_pixels(red)
       print(command)
     elif command == "JOE":
-      client.put_pixels(green)
+      put_pixels(green)
       print(command)
     command = ""
 
